fe3fe70ca52e0afb97ac6cdd892aebde85609993
Michał Pilch <m.pilch2002@gmail.com>
2024-02-16 22:21:03 +0100

    Initial commit

---
+++ src/main/java/pl/sd/repos/ReposApplication.java
@@ -0,0 +1,13 @@
+package pl.sd.repos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sd/repos/ReposApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sd.repos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
